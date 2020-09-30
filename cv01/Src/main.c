e0b7f6f74f62240c78da124f5cd8b5acb7119edf
josefholub <xholub39@vutbr.cz>
2020-09-30 11:20:57 +0200

    cv01 pre-final

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -16,6 +16,7 @@
  *
  ******************************************************************************
  */
+#include "stm32f0xx.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -23,6 +24,17 @@
 
 int main(void)
 {
+	static const uint8_t pole [32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
     /* Loop forever */
-	for(;;);
+	for(;;){
+		for(uint8_t i=0; i<sizeof(pole); i++){
+			if(pole[i]) GPIOA->BSRR = (1<<5);
+			else GPIOA->BRR = (1<<5);
+		}
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+	}
 }
